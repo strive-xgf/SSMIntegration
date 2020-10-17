03645b8ec40fa233779bda26485d7faf5853524b
strive_gf@163.com <strive_gf@163.com>
2020-10-17 09:34:02 +0800

    log4j.properties属性文件配置解读

--- SSM01/src/main/java/com/xgf/dao/impl/PersonDaoImpl.java
+++ SSM01/src/main/java/com/xgf/dao/impl/PersonDaoImpl.java
@@ -4,6 +4,7 @@ import com.xgf.bean.Person;
 import com.xgf.dao.IPersonDao;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -11,7 +12,14 @@ public class PersonDaoImpl implements IPersonDao {
     @Override
     public List<Person> findAll() {
         System.out.println("findAll");
-        return null;
+        //因为没有整合mybatis，这里设置初始化数据进行模拟
+        List<Person> personList = new ArrayList<>();
+
+        Person person1 = new Person(null,"studyday",10000.0);
+        Person person2 = new Person(null,"striveday",20000.0);
+        personList.add(person1);
+        personList.add(person2);
+        return personList;
     }
 
     @Override
--- SSM01/src/test/java/com/xgf/service/TestPersonService.java
+++ SSM01/src/test/java/com/xgf/service/TestPersonService.java
@@ -1,7 +1,7 @@
-package com.xgf.service;//package com.xgf.service;
+package com.xgf.service;
 
 import com.xgf.bean.Person;
-import com.xgf.service.impl.PersonServiceImpl;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
-import java.util.logging.Logger;
+
 
 //用spring自带的test测试
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -22,60 +22,30 @@ public class TestPersonService {
     @Autowired
     IPersonService personService;
 
-
-    //编写两个业务功能，查找所有的Person，保存一个person，一般测试写两个就行
-    //Spring和MyBatis整合，IOC（bean注入）和AOP（事务）测试
-
-    //原始版本写法，改为Spring自带Test，注入进行测试
-//    @Test
-//    public void test01(){
-//        //将结果输出到日志中，以后使用的时候不会通过sout输出到控制台，减少性能损耗
-
-//        //创建业务对象
-//        IPersonService personService = new PersonServiceImpl();
-//
-//        //调用查询
-//        List<Person> list= personService.findAll();
-//        log.info(list);//记录日志
-//        //新增保存
-//        Person person = new Person(null,"DemoGF",200000lf);
-//        int code = personService.saveService(person);
-//        System.out.println("code = " + code);
-//        //调用查询
-//        List<Person> = personService.findAll();
-//        log.info(list);//记录日志
-//
-//    }
-
-
-
-
     //测试IOC DI依赖注入是否成功
     @Test
-    public void test02(){
+    public void test01(){
         System.out.println(personService);
     }
 
-
     //测试查询方法
     @Test
-    public void test03(){
+    public void test02(){
         //记录日志
         log.info(personService+"");
         //查询所有
         List<Person> list = personService.findAll();
         //调用保存
-        log.info("allPerson："+list+"");
+        log.info("allPerson："+list.toString()+"");
     }
 
+    //测试增加方法
     @Test
-    public void test04(){
+    public void test03(){
         Person person = new Person(null,"striveday",20000000.68);
         personService.savePerson(person);
 
         List<Person> list = personService.findAll();
         log.info("allPerson："+list+"");
     }
-
-
 }
